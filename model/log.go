a5add1dcc01c1d032c055c1369edc7ffb010a531
ysicing <i@ysicing.me>
2026-03-20 10:50:03 +0800

    refactor: enable unconditional recording of IP logs
    - Set needRecordIp to true unconditionally for error and consume log functions
    - Commented out the conditional logic for determining whether to record IP logs
    
    Signed-off-by: ysicing <i@ysicing.me>

--- model/log.go
+++ model/log.go
@@ -106,12 +106,12 @@ func RecordErrorLog(c *gin.Context, userId int, channelId int, modelName string,
 	requestId := c.GetString(common.RequestIdKey)
 	otherStr := common.MapToJsonStr(other)
 	// 判断是否需要记录 IP
-	needRecordIp := false
-	if settingMap, err := GetUserSetting(userId, false); err == nil {
-		if settingMap.RecordIpLog {
-			needRecordIp = true
-		}
-	}
+	needRecordIp := true
+	// if settingMap, err := GetUserSetting(userId, false); err == nil {
+	// 	if settingMap.RecordIpLog {
+	// 		needRecordIp = true
+	// 	}
+	// }
 	log := &Log{
 		UserId:           userId,
 		Username:         username,
@@ -167,12 +167,12 @@ func RecordConsumeLog(c *gin.Context, userId int, params RecordConsumeLogParams)
 	requestId := c.GetString(common.RequestIdKey)
 	otherStr := common.MapToJsonStr(params.Other)
 	// 判断是否需要记录 IP
-	needRecordIp := false
-	if settingMap, err := GetUserSetting(userId, false); err == nil {
-		if settingMap.RecordIpLog {
-			needRecordIp = true
-		}
-	}
+	needRecordIp := true
+	// if settingMap, err := GetUserSetting(userId, false); err == nil {
+	// 	if settingMap.RecordIpLog {
+	// 		needRecordIp = true
+	// 	}
+	// }
 	log := &Log{
 		UserId:           userId,
 		Username:         username,
